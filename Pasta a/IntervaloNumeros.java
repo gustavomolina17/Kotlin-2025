87e510170d7449ea99e81cf09e383be11ae6da60
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2025-03-07 11:50:51 -0300

    Update IntervaloNumeros.java

--- Pasta a/IntervaloNumeros.java
+++ Pasta a/IntervaloNumeros.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class IntervaloNumeros {
+public class IntervNumeros {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
